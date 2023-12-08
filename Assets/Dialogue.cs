51d2c97047ef82d3ac6b03f8ec83d9ee4bf09610
ChiZhang9797 <aaronzhang9797@outlook.com>
2023-12-08 10:30:07 -0500

    Added arrow to indicate the current set of films needs to adjust

--- Assets/Dialogue.cs
+++ Assets/Dialogue.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Dialogue : MonoBehaviour
 {
+    private GameObject ArrowGameObject;
+    public GameObject ModelTarget;
     private float Value;
     public GameObject Text1; // assign any text with the script I've made before attached
     public GameObject Text2;
@@ -25,6 +27,27 @@ public class Dialogue : MonoBehaviour
         Script3.TextSetActive(Value); // Use the public method made in the last Script
         TextGuidance Script3_1 = Table1.GetComponent<TextGuidance>(); // Get the script of the text
         Script3_1.TextSetActive(Value); // Use the public method made in the last Script
+
+        if (Value == 2)
+        {
+            ShowArrowAt(-0.1f, 0.075f, 0.05f);
+        }
+        else if (Value == 3)
+        {
+            ShowArrowAt(-0.1f, 0.075f, 0.2f);
+        }
+    }
+
+    private void ShowArrowAt(float x, float y, float z)
+    {
+        var preArrow = GameObject.Find("Arrow(Clone)");
+        if (preArrow != null)
+        {
+            GameObject.Destroy(preArrow);
+        }
+        var arrow = Resources.Load<GameObject>("Arrow");
+        ArrowGameObject = Instantiate(arrow, new Vector3(x, y, z), Quaternion.Euler(0, -90, 0));
+        ArrowGameObject.transform.SetParent(ModelTarget.transform);
     }
 
 }
