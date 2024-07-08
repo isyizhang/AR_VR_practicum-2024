fbe02b4c1e98ed648ff1d46f25f35e9c45dbfd88
itroye <08280828zy@gmail.com>
2024-07-07 23:39:11 -0400

    decrease the items

--- Assets/Scripts/FoodItemDatabase.cs
+++ Assets/Scripts/FoodItemDatabase.cs
@@ -22,22 +22,22 @@ public static class FoodItemDatabase
 
         new Item("candy", new float[] { 1.0f, 0f, 0.3f }),
         new Item("coke", new float[] { 1f, 0.1f, 0f }),
-        new Item("milkChocolate", new float[] { 0.9f, 0.1f, 0.5f }),
-        new Item("chocolateCake", new float[] { 0.8f, 0.1f, 0.7f }),
+        //new Item("milkChocolate", new float[] { 0.9f, 0.1f, 0.5f }),
+        new Item("chocolateCake", new float[] { 0.8f, 0.1f, 0.6f }),
         new Item("iceCream", new float[] { 0.9f, 0f, 0.5f }),
-        new Item("cookies", new float[] { 0.7f, 0.1f, 0.7f }),
-        new Item("ham", new float[] { 0f, 0.7f, 0.8f }),
+        //new Item("cookies", new float[] { 0.8f, 0.1f, 0.6f }),
+        //new Item("ham", new float[] { 0f, 0.7f, 0.8f }),
         new Item("pizza", new float[] { 0.1f, 0.7f, 0.7f }),
-        new Item("macAndCheese", new float[] { 0.1f, 0.7f, 0.8f }),
+        //new Item("macAndCheese", new float[] { 0.1f, 0.7f, 0.8f }),
         new Item("frenchFries", new float[] { 0.1f, 0.8f, 0.9f }),
-        new Item("popcorn", new float[] { 0.1f, 0.8f, 0.6f }),
+        //new Item("popcorn", new float[] { 0.1f, 0.8f, 0.6f }),
         new Item("steak", new float[] { 0.2f, 0.6f, 0.8f }),
         new Item("lettuce", new float[] { 0.1f, 0.1f, 0f }),
         new Item("milk", new float[] { 0.4f, 0f, 0.5f }),
         new Item("apple", new float[] { 0.7f, 0f, 0f }),
-        new Item("egg", new float[] { 0.1f, 0.2f, 0.4f }),
+        new Item("egg", new float[] { 0.1f, 0.1f, 0.2f }),
         new Item("broccoli", new float[] { 0.2f, 0.1f, 0f }),
-        new Item("fruitYogurt", new float[] { 0.7f, 0f, 0.4f })
+        //new Item("fruitYogurt", new float[] { 0.7f, 0f, 0.4f })
     };
 }
 
