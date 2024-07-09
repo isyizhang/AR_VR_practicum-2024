b0ad885806f91357b25898843eadd274ec3c6c36
itroye <08280828zy@gmail.com>
2024-07-09 10:04:03 -0400

    edit light path

--- Assets/Scripts/FoodItemDatabase.cs
+++ Assets/Scripts/FoodItemDatabase.cs
@@ -23,7 +23,7 @@ public static class FoodItemDatabase
         new Item("candy", new float[] { 1.0f, 0f, 0.3f }),
         new Item("coke", new float[] { 1f, 0.1f, 0f }),
         //new Item("milkChocolate", new float[] { 0.9f, 0.1f, 0.5f }),
-        new Item("chocolateCake", new float[] { 0.8f, 0.1f, 0.6f }),
+        new Item("chocolateCake", new float[] { 0.9f, 0.1f, 0.6f }),
         new Item("iceCream", new float[] { 0.9f, 0f, 0.5f }),
         //new Item("cookies", new float[] { 0.8f, 0.1f, 0.6f }),
         //new Item("ham", new float[] { 0f, 0.7f, 0.8f }),
@@ -36,7 +36,7 @@ public static class FoodItemDatabase
         new Item("milk", new float[] { 0.4f, 0f, 0.5f }),
         new Item("apple", new float[] { 0.7f, 0f, 0f }),
         new Item("egg", new float[] { 0.1f, 0.1f, 0.2f }),
-        new Item("broccoli", new float[] { 0.2f, 0.1f, 0f }), 
+        new Item("broccoli", new float[] { 0.1f, 0.1f, 0f }), 
         //new Item("fruitYogurt", new float[] { 0.7f, 0f, 0.4f })
     };
 }
